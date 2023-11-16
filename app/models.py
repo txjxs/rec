bee42c2cd39a5617ae8389d512779f61fcb54a1a
txjxs <tejasnisar2000@gmail.com>
2023-11-17 05:02:51 +0530

    solved minor import errors

--- app/models.py
+++ app/models.py
@@ -6,7 +6,10 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    # Add any other user fields as needed
+
+    def __init__(self, username, password_hash):
+        self.username = username
+        self.password_hash = password_hash
 
 @login_manager.user_loader
 def load_user(id):
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import app, login_manager, db
 from app.models import User
 
@@ -10,6 +11,7 @@ from app.models import User
 def index():
     return render_template('base.html')
 
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
@@ -17,8 +19,8 @@ def signup():
         username = request.form['username']
         password = request.form['password']
         
-        # Example: hash the password (use a secure method in production)
-        hashed_password = hash_password(password)
+        # Hash the password securely
+        hashed_password = generate_password_hash(password, method='sha256')
 
         new_user = User(username=username, password_hash=hashed_password)
         db.session.add(new_user)
@@ -28,9 +30,25 @@ def signup():
         return redirect(url_for('login'))
     return render_template('signup.html')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # Implement login logic
+    if request.method == 'POST':
+        # Get data from the form
+        username = request.form['username']
+        password = request.form['password']
+
+        # Query the user from the database
+        user = User.query.filter_by(username=username).first()
+
+        # Check if the user exists and the password is correct
+        if user and check_password_hash(user.password_hash, password):
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for('profile'))
+        else:
+            flash('Login unsuccessful. Please check your username and password.', 'danger')
+
     return render_template('login.html')
 
 @app.route('/profile')
